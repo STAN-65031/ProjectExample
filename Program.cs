307804043312d7e19acba9243cdbee3b8ccb9ecf
VitalyOv <vit2013aly@mail.ru>
2022-08-09 17:32:24 +0300

    Add telegram bot example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+var botClient = new TelegramBotClient("5276823851:AFF3LjRMF7DOir1eeIgwINI6oyYJpIjCQuM");
+
+var me = await botClient.GetMeAsync();
+
+while (true)
+{
+    var updates = await botClient.GetUpdatesAsync();
+    for (int i = 0; i < updates.Count(); i++)
+    {
+        switch(updates[i].Type)
+        {
+            case Telegram.Bot.Types.Enums.UpdateType.Message: {
+                UpdateHandle(updates[i].Message!);
+                updates = await botClient.GetUpdatesAsync(updates[^1].Id + 1);
+                break;
+            }
+        }
+    }
+}
+
+async void UpdateHandle(Message message){
+    await botClient!.SendTextMessageAsync(message.Chat.Id, $"{message.From.Username} Ты мне написал {message.Text}?");
+}
